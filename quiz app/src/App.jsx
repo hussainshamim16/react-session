0b0086c7154ced8aa4e94633b9232be2e4ff56ec
muhammad hussain <hj422748@gmail.com>
2024-09-05 22:59:21 +0500

    last last stage

--- quiz app/src/App.jsx
+++ quiz app/src/App.jsx
@@ -3,48 +3,86 @@ import { useState } from 'react';
 
 const App = () => {
 
-  const [quiestion, setquiestion] = useState(0);
-  const [score, setscore] = useState(0)
-  const [result, setresult] = useState(false)
+  let [quiestion, setquiestion] = useState(0);
+  let [score, setscore] = useState(0)
+  let [result, setresult] = useState(false)
+
+const hover = ()=>{}
 
 
-  const quiestionsApi = [
+  let quiestionsApi = [
     {
       id: 1,
-      quiestion: "html stand for !",
+      quiestionn: "html stand for !",
       options: ["hyper texter markup language", "hyper text mekup language", "hyper text markup language", "hello text markup language"],
       answer: "hyper text markup language"
     },
     {
       id: 2,
-      quiestion: "css stand for !",
+      quiestionn: "css stand for !",
       options: ["caseer cading style sheet", "case cooding style sheet", "case cading style shepreat", "case cading style sheet"],
       answer: "case cading style sheet"
     },
   ]
 
-  console.log(quiestionsApi[0].answer)
+  // console.log(quiestionsApi[0].answer)
   const nextbtn = (elem) => {
     if (elem.target.value === quiestionsApi[quiestion].answer) {
       console.log("yes")
     }
     // console.log("input value", elem.target.value)
   }
-  
+
   return <div>
-    <h1>Html Quiz app</h1>
+    {/* <h1>Html Quiz app</h1> */}
     {/* {console.log(quiestionsApi)} */}
-    <form action="#">
-      {/* <input type="text" onChange={nextbtn}/> */}
-      {/* <button onClick={nextbtn}>button</button> */}
-    </form>
+    {/* <form action="#"> */}
+    {/* <input type="text" onChange={nextbtn}/> */}
+    {/* <button onClick={nextbtn}>button</button> */}
+    {/* </form> */}
+
+    <div className="quizSection">
+      <h1 id="quiestioner">
+        {quiestionsApi[quiestion].id + " "}{quiestionsApi[quiestion].quiestionn}
+      </h1>
+      <ul>
+      <ol id="optioner">
+        {/* <li>  {quiestionsApi[quiestion].options}</li> */}
+
+        {/* {quiestionsApi[quiestion].map((Val,ind,arr)=>{
+          console.log()
+        })} */}
+
+      {
+        quiestionsApi.map((v ,i ,a)=>{
+          // console.log(quiestionsApi[quiestion].options[i])
+         
+          return <li key={i} onClick={hover}>{quiestionsApi[quiestion].options[i]}</li>
+          // console.log( <li key={i}>ali</li>)
+          // `<li key={i}>ali</li>`
+          
+          // console.log(i)
+        })
+        
+      }
+      </ol>
+        {
+          
+          // quiestionsApi.map((v,i,a)=>{
+            
+          //   return <li>hello</li>
+          // })
+          
+          }
+      </ul>
+    </div>
   </div>
 
 }
 
 
-    let QuizUi = () => {
-      <h1>QuizUi</h1>
-    }
+// let QuizUi = () => {
+//   <h1>QuizUi</h1>
+// }
 
 export default App
