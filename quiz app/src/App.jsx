b2b5287533ee953823b964dcce6b22595d4ffc09
muhammad hussain <hj422748@gmail.com>
2024-09-06 08:13:14 +0500

    done quiz

--- quiz app/src/App.jsx
+++ quiz app/src/App.jsx
@@ -1,88 +1,109 @@
 import React from 'react'
 import { useState } from 'react';
+import QuizUI from './components/QuizUI'
 
-const App = () => {
+// const App = () => {
 
-  let [quiestion, setquiestion] = useState(0);
-  let [score, setscore] = useState(0)
-  let [result, setresult] = useState(false)
+//   let [quiestion, setquiestion] = useState(0);
+//   let [score, setscore] = useState(0)
+//   let [result, setresult] = useState(false)
 
-const hover = ()=>{}
+// const hover = ()=>{}
 
 
-  let quiestionsApi = [
-    {
-      id: 1,
-      quiestionn: "html stand for !",
-      options: ["hyper texter markup language", "hyper text mekup language", "hyper text markup language", "hello text markup language"],
-      answer: "hyper text markup language"
-    },
-    {
-      id: 2,
-      quiestionn: "css stand for !",
-      options: ["caseer cading style sheet", "case cooding style sheet", "case cading style shepreat", "case cading style sheet"],
-      answer: "case cading style sheet"
-    },
-  ]
+//   let quiestionsApi = [
+//     {
+//       id: 1,
+//       quiestionn: "html stand for !",
+//       options: ["hyper texter markup language", "hyper text mekup language", "hyper text markup language", "hello text markup language"],
+//       answer: "hyper text markup language"
+//     },
+//     {
+//       id: 2,
+//       quiestionn: "css stand for !",
+//       options: ["caseer cading style sheet", "case cooding style sheet", "case cading style shepreat", "case cading style sheet"],
+//       answer: "case cading style sheet"
+//     },
+//   ]
 
-  // console.log(quiestionsApi[0].answer)
-  const nextbtn = (elem) => {
-    if (elem.target.value === quiestionsApi[quiestion].answer) {
-      console.log("yes")
-    }
-    // console.log("input value", elem.target.value)
-  }
+//   // console.log(quiestionsApi[0].answer)
+//   const nextbtn = (elem) => {
+//     if (elem.target.value === quiestionsApi[quiestion].answer) {
+//       console.log("yes")
+//     }
+//     // console.log("input value", elem.target.value)
+//   }
 
-  return <div>
-    {/* <h1>Html Quiz app</h1> */}
-    {/* {console.log(quiestionsApi)} */}
-    {/* <form action="#"> */}
-    {/* <input type="text" onChange={nextbtn}/> */}
-    {/* <button onClick={nextbtn}>button</button> */}
-    {/* </form> */}
+//   return <div>
+//     {/* <h1>Html Quiz app</h1> */}
+//     {/* {console.log(quiestionsApi)} */}
+//     {/* <form action="#"> */}
+//     {/* <input type="text" onChange={nextbtn}/> */}
+//     {/* <button onClick={nextbtn}>button</button> */}
+//     {/* </form> */}
 
-    <div className="quizSection">
-      <h1 id="quiestioner">
-        {quiestionsApi[quiestion].id + " "}{quiestionsApi[quiestion].quiestionn}
-      </h1>
-      <ul>
-      <ol id="optioner">
-        {/* <li>  {quiestionsApi[quiestion].options}</li> */}
+//     <div className="quizSection">
+//       <h1 id="quiestioner">
+//         {quiestionsApi[quiestion].id + " "}{quiestionsApi[quiestion].quiestionn}
+//       </h1>
+//       <ul>
+//       <ol id="optioner">
+//         {/* <li>  {quiestionsApi[quiestion].options}</li> */}
 
-        {/* {quiestionsApi[quiestion].map((Val,ind,arr)=>{
-          console.log()
-        })} */}
+//         {/* {quiestionsApi[quiestion].map((Val,ind,arr)=>{
+//           console.log()
+//         })} */}
 
-      {
-        quiestionsApi.map((v ,i ,a)=>{
-          // console.log(quiestionsApi[quiestion].options[i])
+//       {
+//         quiestionsApi.map((v ,i ,a)=>{
+//           // console.log(quiestionsApi[quiestion].options[i])
          
-          return <li key={i} onClick={hover}>{quiestionsApi[quiestion].options[i]}</li>
-          // console.log( <li key={i}>ali</li>)
-          // `<li key={i}>ali</li>`
+//           return <li key={i} onClick={hover}>{quiestionsApi[quiestion].options[i]}</li>
+//           // console.log( <li key={i}>ali</li>)
+//           // `<li key={i}>ali</li>`
           
-          // console.log(i)
-        })
+//           // console.log(i)
+//         })
         
-      }
-      </ol>
-        {
+//       }
+//       </ol>
+//         {
           
-          // quiestionsApi.map((v,i,a)=>{
+//           // quiestionsApi.map((v,i,a)=>{
             
-          //   return <li>hello</li>
-          // })
+//           //   return <li>hello</li>
+//           // })
           
-          }
-      </ul>
-    </div>
-  </div>
+//           }
+//       </ul>
+//     </div>
+//   </div>
 
-}
-
-
-// let QuizUi = () => {
-//   <h1>QuizUi</h1>
 // }
 
-export default App
+
+// // let QuizUi = () => {
+// //   <h1>QuizUi</h1>
+// // }
+
+// export default App
+
+
+// import React, { useState } from 'react';
+// import Quiz from './Quiz';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{
+        textAlign: "center",
+        color:"black",
+        fontSize:"3em"
+      }}>HTML Beginner Quiz</h1>
+      <QuizUI />
+    </div>
+  );
+}
+
+export default App;
--- quiz app/src/components/QuizUI.jsx
+++ quiz app/src/components/QuizUI.jsx
@@ -1,13 +1,205 @@
-import React from 'react'
+// import React from 'react'
 
-const QuizUI = (quiestion,options) => {
-    return <div>
-        <h1>{quiestion}</h1>
-        <ul>
-            <li>{options}</li>
-        </ul>
-    </div>
+// const QuizUI = (quiestion,options) => {
+//     return <div>
+//         <h1>{quiestion}</h1>
+//         <ul>
+//             <li>{options}</li>
+//         </ul>
+//     </div>
 
+// }
+
+// export default QuizUI
+
+
+import React, { useState } from 'react';
+import './quizUi.css';
+
+const questions =  [
+    {
+      questionText: "What does HTML stand for?",
+      answerOptions: [
+        { answerText: "Hyper Text Markup Language", isCorrect: true },
+        { answerText: "Hyperlinks and Text Markup Language", isCorrect: false },
+        { answerText: "Home Tool Markup Language", isCorrect: false },
+        { answerText: "Hyperlink Text Management Language", isCorrect: false }
+      ]
+    },
+    {
+      questionText: "What is the first tag in an HTML document?",
+      answerOptions: [
+        { answerText: "<html>", isCorrect: false },
+        { answerText: "<body>", isCorrect: false },
+        { answerText: "<!DOCTYPE>", isCorrect: true },
+        { answerText: "<head>", isCorrect: false }
+      ]
+    },
+    {
+      questionText: "Which HTML tag is used to insert a line break?",
+      answerOptions: [
+        { answerText: "<br>", isCorrect: true },
+        { answerText: "<lb>", isCorrect: false },
+        { answerText: "<break>", isCorrect: false },
+        { answerText: "<newline>", isCorrect: false }
+      ]
+    },
+    {
+      questionText: "Which HTML tag is used to display an image?",
+      answerOptions: [
+        { answerText: "<img>", isCorrect: true },
+        { answerText: "<picture>", isCorrect: false },
+        { answerText: "<image>", isCorrect: false },
+        { answerText: "<src>", isCorrect: false }
+      ]
+    },
+    {
+      questionText: "Which HTML tag defines the title of a document?",
+      answerOptions: [
+        { answerText: "<title>", isCorrect: true },
+        { answerText: "<meta>", isCorrect: false },
+        { answerText: "<head>", isCorrect: false },
+        { answerText: "<header>", isCorrect: false }
+      ]
+    },
+    {
+      questionText: "Which HTML tag is used to define an input field in a form?",
+      answerOptions: [
+        { answerText: "<input>", isCorrect: true },
+        { answerText: "<form>", isCorrect: false },
+        { answerText: "<field>", isCorrect: false },
+        { answerText: "<textbox>", isCorrect: false }
+      ]
+    },
+    {
+      questionText: "Which HTML tag is used for creating headings?",
+      answerOptions: [
+        { answerText: "<h1> to <h6>", isCorrect: true },
+        { answerText: "<header>", isCorrect: false },
+        { answerText: "<head>", isCorrect: false },
+        { answerText: "<h>", isCorrect: false }
+      ]
+    },
+    {
+      questionText: "Which HTML attribute is used to define inline styles?",
+      answerOptions: [
+        { answerText: "style", isCorrect: true },
+        { answerText: "class", isCorrect: false },
+        { answerText: "id", isCorrect: false },
+        { answerText: "styles", isCorrect: false }
+      ]
+    },
+    {
+      questionText: "Which HTML tag is used to create a hyperlink?",
+      answerOptions: [
+        { answerText: "<a>", isCorrect: true },
+        { answerText: "<link>", isCorrect: false },
+        { answerText: "<href>", isCorrect: false },
+        { answerText: "<anchor>", isCorrect: false }
+      ]
+    },
+    {
+      questionText: "Which HTML element is used for inserting a comment?",
+      answerOptions: [
+        { answerText: "<!-- comment -->", isCorrect: true },
+        { answerText: "<comment>", isCorrect: false },
+        { answerText: "// comment", isCorrect: false },
+        { answerText: "# comment", isCorrect: false }
+      ]
+    }
+  ];
+  ;
+
+function QuizUI() {
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [score, setScore] = useState(0);
+    const [showScore, setShowScore] = useState(false);
+
+    const handleAnswerOptionClick = (isCorrect) => {
+        if (isCorrect) {
+            setScore(score + 1);
+        }
+
+        const nextQuestion = currentQuestion + 1;
+        if (nextQuestion < questions.length) {
+            setCurrentQuestion(nextQuestion);
+        } else {
+            setShowScore(true);
+        }
+    };
+
+    const ret = () => {
+        location.reload()
+    }
+
+    return (
+        <div className="quiz">
+            {showScore ? (
+                <div className="score-section">
+                    your Score: {score} out of {questions.length}
+                    <button onClick={ret}>return</button>
+                </div>
+            ) : (
+                <>
+                    <div className="question-section">
+                        <div className="question-count">
+                            <span>Question {currentQuestion + 1}</span>/{questions.length}
+                        </div>
+                        <div className="question-text">{questions[currentQuestion].questionText}</div>
+                    </div>
+                    <div className="answer-section">
+                        {questions[currentQuestion].answerOptions.map((answerOption, index) => (
+                            <button key={index} onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>
+                                {answerOption.answerText}
+                            </button>
+                        ))}
+                    </div>
+                </>
+            )}
+        </div>
+    );
 }
 
-export default QuizUI
+// function QuizUI(params) {
+
+//     let [quiestioncounter, setQuestioncounter] = useState(0)
+//     let [result, setResult] = useState(false)
+//     let [score, setScore] = useState(0)
+
+//     if (quiestioncounter == questions.length) {
+//         alert('complete')
+
+//         return
+//       }
+
+//     //   const nextQuiestion = setQuestioncounter(quiestioncounter+1)
+//     //   if (quiestioncounter == questions.lenght) {
+//     //     console.log("render true")
+//     //   }
+
+//     const anserChenker = (ele) => {
+//         console.log(ele, 'ui')
+//         setQuestioncounter(quiestioncounter + 1)
+//         if (ele === true) {
+//             setScore(score + 1 )
+//             console.log(score)
+//         }
+
+//     }
+
+//     return <div className="ui">
+//         <h3>{questions[quiestioncounter].questionText}</h3>
+//         <h4>quiestions : {quiestioncounter}/{questions.length}</h4>
+//         <ol>
+//             {
+//                 questions[quiestioncounter].answerOptions.map((val, ind) => {
+//                     // console.log()
+//                     return <button key={ind} onClick={()=>anserChenker(val.isCorrect)}>{val.answerText}</button>
+//                 })
+//             }
+//         </ol>
+//     </div>
+
+// }
+
+export default QuizUI;
