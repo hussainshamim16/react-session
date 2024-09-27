c76efe029f6b5253aa39cafa60844e6711b68c74
hussainshamim16 <hj422748@gmail.com>
2024-09-27 04:45:07 -0700

    all chat gpt is remove

--- todo-redux/src/App.jsx
+++ todo-redux/src/App.jsx
@@ -4,73 +4,103 @@ import { useDispatch, useSelector } from 'react-redux';
 import { nanoid } from '@reduxjs/toolkit';
 
 const App = () => {
-  // ref for input field
-  const inputVal = useRef();
-  const [editMode, setEditMode] = useState(null);  // To manage edit mode
-  const [editInputValue, setEditInputValue] = useState('');  // Edit value
+  // // ref for input field
+  // const inputVal = useRef();
 
-  // dispatch to trigger redux actions
-  const dispatch = useDispatch();
+
+
+  // // dispatch to trigger redux actions
+  // const dispatch = useDispatch();
   
-  // select todo data from Redux store
-  const todoData = useSelector((state) => state.todo.todo);
+  // // select todo data from Redux store
+  // const todoData = useSelector((state) => state.todo.todo);
 
-  // handle adding task
+  // // handle adding task
+  // const addTask = (event) => {
+  //   if(!inputVal.value){
+  //     alert("Enter Correct Todo")
+  //     return
+  //   }
+  //   event.preventDefault();
+  //   dispatch(add({
+  //     title: inputVal.current.value
+  //   }));
+  //   inputVal.current.value = '';
+  // }
+
+  // // handle removing task
+  // const removeTask = (id) => {
+  //   dispatch(remove(id));
+  // }
+
+  // // handle editing task
+  // const startEdit = (id, currentTitle) => {
+  //   setEditMode(id);
+  //   setEditInputValue(currentTitle);
+  // }
+
+  // const saveEdit = (id) => {
+  //   dispatch(edit({ id, newTitle: editInputValue }));
+  
+  // }
+
+
+  const inputVal = useRef()
+  //dispatch
+  const dispatch = useDispatch()
+  //state
+  const todoData = useSelector((state) => state.todo.todo)
+  //on changing of todoData
   const addTask = (event) => {
-    if(!inputVal.value){
-      alert("Enter Correct Todo")
-      return
-    }
-    event.preventDefault();
+    event.preventDefault()
     dispatch(add({
       title: inputVal.current.value
-    }));
-    inputVal.current.value = '';
-  }
-
-  // handle removing task
-  const removeTask = (id) => {
-    dispatch(remove(id));
-  }
-
-  // handle editing task
-  const startEdit = (id, currentTitle) => {
-    setEditMode(id);
-    setEditInputValue(currentTitle);
-  }
-
-  const saveEdit = (id) => {
-    dispatch(edit({ id, newTitle: editInputValue }));
-    setEditMode(null);  // Exit edit mode
+    }))
+    // console.log(todoData)
+    inputVal.current.value = ''
   }
 
   return (
-    <>
-      <h1>Create Task</h1>
-      <input type="text" placeholder='Add task' ref={inputVal} />
-      <button onClick={addTask}>Add Task</button>
+    // <>
+    //   <h1>Create Task</h1>
+    //   <input type="text" placeholder='Add task' ref={inputVal} />
+    //   <button onClick={addTask}>Add Task</button>
 
+    //   <ul>
+    //     {todoData.map((item) => (
+    //       <li key={item.id}>
+    //         {editMode === item.id ? (
+    //           <>
+    //             <input 
+    //               type="text" 
+    //               value={editInputValue} 
+    //               onChange={(e) => setEditInputValue(e.target.value)}
+    //             />
+    //             <button onClick={() => saveEdit(item.id)}>Save</button>
+    //           </>
+    //         ) : (
+    //           <>
+    //             {item.title}
+    //             <button onClick={() => startEdit(item.id, item.title)}>Edit</button>
+    //             <button onClick={() => removeTask(item.id)}>Delete</button>
+    //           </>
+    //         )}
+    //       </li>
+    //     ))}
+    //   </ul>
+    // </>
+    <>
+     <h1>create task</h1>
+      <input type="text" placeholder='add task' ref={inputVal} />
+      <button onClick={() => addTask(event)}>add task</button>
       <ul>
-        {todoData.map((item) => (
-          <li key={item.id}>
-            {editMode === item.id ? (
-              <>
-                <input 
-                  type="text" 
-                  value={editInputValue} 
-                  onChange={(e) => setEditInputValue(e.target.value)}
-                />
-                <button onClick={() => saveEdit(item.id)}>Save</button>
-              </>
-            ) : (
-              <>
-                {item.title}
-                <button onClick={() => startEdit(item.id, item.title)}>Edit</button>
-                <button onClick={() => removeTask(item.id)}>Delete</button>
-              </>
-            )}
-          </li>
-        ))}
+        {console.log("ali bad",todoData)}
+        {
+          todoData.map((value, index) => {
+            // console.log("id",index)
+           return <li key={index}>{value.title} <button onClick={()=>dispatch(edit(index))}>edit</button> <button onClick={()=>dispatch(remove(index))}>Delete</button></li>
+          })
+        }
       </ul>
     </>
   );
--- todo-redux/src/config/redux/todoSlice.js
+++ todo-redux/src/config/redux/todoSlice.js
@@ -1,34 +1,62 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+// import { createSlice, nanoid } from "@reduxjs/toolkit";
 
+// const todoSlice = createSlice({
+//     name: "todo",
+//     initialState: {
+//         todo: []
+//     },
+//     reducers: {
+//         add: (state, action) => {
+//             state.todo.push({
+//                 id: nanoid(),
+//                 title: action.payload.title
+//             });
+//         },
+
+//         edit: (state, action) => {
+//             const { id, newTitle } = action.payload;
+//             const todoItem = state.todo.find(item => item.id === id);
+//             if (todoItem) {
+//                 todoItem.title = newTitle;  // Update the todo title
+//             }
+//         },
+
+//         remove: (state, action) => {
+//             const index = state.todo.findIndex(item => item.id === action.payload);
+//             if (index !== -1) {
+//                 state.todo.splice(index, 1);  // Remove the todo item
+//             }
+//         }
+//     }
+// });
+
+// export const { add, edit, remove } = todoSlice.actions;
+// export default todoSlice.reducer;
+
+
+import { createSlice, nanoid } from "@reduxjs/toolkit";
 const todoSlice = createSlice({
-    name: "todo",
-    initialState: {
+    name: "todo"
+    , initialState: {
         todo: []
-    },
-    reducers: {
+    }
+    , reducers: {
         add: (state, action) => {
+            // console.log("action.payload.title")
             state.todo.push({
                 id: nanoid(),
                 title: action.payload.title
-            });
+            })
         },
-
         edit: (state, action) => {
-            const { id, newTitle } = action.payload;
-            const todoItem = state.todo.find(item => item.id === id);
-            if (todoItem) {
-                todoItem.title = newTitle;  // Update the todo title
-            }
+            console.log("edited",state.todo.splice(action.payload,1,1))
+            // const editVal = prompt("Enter Your Edited Value")
+            // console.log(editVal)
         },
-
         remove: (state, action) => {
-            const index = state.todo.findIndex(item => item.id === action.payload);
-            if (index !== -1) {
-                state.todo.splice(index, 1);  // Remove the todo item
-            }
+            console.log(state.todo.splice(action.payload,1))
         }
     }
-});
-
-export const { add, edit, remove } = todoSlice.actions;
-export default todoSlice.reducer;
+})
+export const { add, edit, remove } = todoSlice.actions
+export default todoSlice.reducer
